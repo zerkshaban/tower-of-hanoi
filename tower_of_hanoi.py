b33792af768c3367caaa06ce2cd180be5409a5dc
zerkshaban <60327014+zerkshaban@users.noreply.github.com>
2020-02-16 23:30:06 +0500

    minor changes

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -1,7 +1,10 @@
-tower(n, fr, spare, to):
+def tower(n, fr, spare, to):
     if n == 1 :
-        print("move" + str(fr)+" to "+str(to))
+        print("move " + str(fr)+" to "+str(to))
     else:
         tower(n-1, fr, spare, to)
         tower(1, fr, to, spare)
         tower(n-1, spare, to, fr)
+
+
+tower(5,'f','t','s')
