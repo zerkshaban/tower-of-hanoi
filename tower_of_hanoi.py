855782b1617521654c5c77549bb47416b4a229db
zerkshaban <60327014+zerkshaban@users.noreply.github.com>
2020-02-16 22:41:54 +0500

    code for the problem

---
+++ tower_of_hanoi.py
@@ -0,0 +1,7 @@
+tower(n, fr, spare, to):
+    if n == 1 :
+        print("move" + str(fr)+" to "+str(to))
+    else:
+        tower(n-1, fr, spare, to)
+        tower(1, fr, to, spare)
+        tower(n-1, spare, to, fr)
